c4ce55103c7463a5b234dbc9d97dea2d8274cc2c
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-02-28 16:51:02 +0800

    feat: add date-fns dependency and implement date comparison for applicant status updates in cron job

--- src/controllers/cronjob/updateStatus.js
+++ src/controllers/cronjob/updateStatus.js
@@ -1,7 +1,6 @@
 const cron = require("node-cron")
 const pool = require("../../config/db");
-
-
+const { differenceInMonths } = require("date-fns");
 const getBlackListedApplicants = async () => {
     const sql = `
                 SELECT * 
@@ -20,34 +19,85 @@ const getBlackListedApplicants = async () => {
     }
 }
 
-const updateStatus = () => {
+// const updateStatus = (applicant) => {
+//     const sql = `
+//         UPDATE applications_progress 
+//         SET stage='PRE-SCREENING', status='NONE', blacklisted_type='null', reason='null'
 
+//     `;
+// }
+
+const updateStatus = async (applicant) => {
+    try {
+        const sql = `
+            UPDATE applications_progress 
+            SET stage=?, status=?, blacklisted_type=?, reason=?
+            WHERE progress_id = ?
+        `;
+        const values = ['PRE-SCREENING', 'NONE', null, null, applicant.progress_id];
+        await pool.execute(sql, values);
+
+        return true
+    } catch (error) {
+        console.log(error);
+        return false
+    }
 }
 
-const checkStatus = () => {
-    
+const checkDateElapsed = (applicant) => {
+    try {
+        if (applicant.blacklisted_type == "SOFT") {
+            //differences in dates
+            const updated_at = new Date(applicant.updated_at);
+            const current_date = new Date.now();
+            const difference = differenceInMonths(current_date, updated_at);
+
+            if (difference >= 6) {
+                return true
+            }
+        }
+        else if (applicant.blacklisted_type == "HARD") {
+            //differences in dates
+            const updated_at = new Date(applicant.updated_at);
+            const current_date = new Date.now();
+            const difference = differenceInMonths(current_date, updated_at);
+
+            if (difference >= 12) {
+               return true
+            }
+        }
+        else {
+            return false;
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const updateStatusCronJob = () => {
-    //runs every minute
-    // cron.schedule("* * * * *", () => {
-    //     console.log("scheduled job is runnning.")
-    // });
+    /*
+    get blacklisted applicants
+    check 
+        if 6 or 12 months has elapsed
+            update 
+            add to notification
+            sent email
+
+    */
 
     //runs everyday at midnight
-    cron.schedule("0 0 * * *", () => {
+    cron.schedule("0 0 * * *", async () => {
         console.log("scheduled job is runnning...");
+        const blacklistedApplicants = await getBlackListedApplicants();
 
-        
         //check
-        
-        
-        //if updated
-            //add to notification
-            
-            //email
-
-        //
+        blacklistedApplicants.forEach(applicant => {
+            if (checkDateElapsed(applicant)) {
+                updateStatus(applicant);
+                //sent to notification
+                //sent email
+            }
+        });
     });
 }
 
