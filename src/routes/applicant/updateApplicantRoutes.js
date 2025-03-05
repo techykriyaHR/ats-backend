a615c9d25c86c07833f14a6895396011307d1b4e
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-03-05 23:04:20 +0800

    chore:change filename for clarity

--- src/routes/applicant/updateApplicantRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-require("dotenv").config()
-const router = express.Router();
-
-const updateApplicantStatus = require("../../controllers/applicant/updateApplicantStatusControllers")
-
-router.get("/", updateApplicantStatus.updateApplicantStatus);
-
-module.exports = router;
