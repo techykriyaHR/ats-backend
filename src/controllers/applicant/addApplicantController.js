71c04c0d67a8ca586f7a4ec196974feb02637716
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-03-21 16:28:50 +0800

    refactor: enhance blacklisted applicant check logic and improve email notifications

--- src/controllers/applicant/addApplicantController.js
+++ src/controllers/applicant/addApplicantController.js
@@ -249,53 +249,8 @@ exports.uploadApplicants = [
 ];
 
 
-const getBlackListedApplicants = async () => {
-    const sql = `
-                SELECT * 
-                FROM ats_applicants a
-                INNER JOIN ats_applicant_trackings at USING (applicant_id)
-                INNER JOIN ats_applicant_progress ap USING (progress_id)
-                WHERE ap.status = 'BLACKLISTED';
-    `;
 
-    try {
-        const [results, fields] = await pool.execute(sql);
-        return results
-    } catch (error) {
-        console.error(error);
-        return [];
-    }
-}
 
-// const checkInBlacklisted = (applicant, blackListedApplicants) => {
-//     array.forEach(blacklisted => {
-//         if (
-//             applicant.first_name === blacklisted.first_name &&
-//             applicant.last_name === blacklisted.last_name &&
-//             applicant.email_1 === blacklisted.email_1 &&
-//             applicant.mobile_number_1 === blacklisted.mobile_number_1 &&
-//             applicant.birth_date === blacklisted.birth_date
-//         ) {
-
-//         }
-//     });
-// }
-
-const checkInBlacklisted = (applicant, blackListedApplicants) => {
-    const isBlacklisted = false; 
-    blackListedApplicants.forEach(blacklisted => {
-        if (
-            applicant.first_name === blacklisted.first_name &&
-            applicant.last_name === blacklisted.last_name &&
-            applicant.email_1 === blacklisted.email_1 &&
-            applicant.mobile_number_1 === blacklisted.mobile_number_1 &&
-            applicant.birth_date === blacklisted.birth_date
-        ) {
-            isBlacklisted = true; 
-        }
-    });
-    return isBlacklisted; 
-}
 
 exports.checkIfBlacklisted = async (req, res) => {
     try {
@@ -309,17 +264,17 @@ exports.checkIfBlacklisted = async (req, res) => {
             //email the applicant
             const email_body = `
             <p>
-                You're blacklisted
+                Upon checking our system, you're blacklisted. You can apply once your blacklisted status is lifted. Thank you!
             </p>`;
-            const email_subject = `Blacklisted`;
+            const email_subject = `Application to FullSuite has Failed`;
 
             emailController.emailApplicantGuest(applicant, email_subject, email_body);
 
             //return true
-            return res.status(200).json({ isBlacklisted: isBlacklisted, message: "ok", emailMessage: "..." });
+            return res.status(200).json({ isBlacklisted: isBlacklisted, message: "ok", emailMessage: email_body});
         }
 
-        return res.status(200).json({ isBlacklisted: isBlacklisted, message: "ok", emailMessage: "...not " });
+        return res.status(200).json({ isBlacklisted: isBlacklisted, message: "ok" });
     } catch (error) {
         res.status(500).json({ message: error.message })
 
