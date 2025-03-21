0bc2a9c8a9f6a6a7476344b75613401f1d3dcfad
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-03-21 16:30:32 +0800

    fix: fix the error relating to the checking of blacklisted applicants.

--- src/controllers/applicant/addApplicantController.js
+++ src/controllers/applicant/addApplicantController.js
@@ -249,7 +249,44 @@ exports.uploadApplicants = [
 ];
 
 
+const getBlackListedApplicants = async () => {
+    const sql = `
+                SELECT *
+                FROM ats_applicants a
+                LEFT JOIN ats_contact_infos c
+                    ON a.applicant_id = c.applicant_id
+                LEFT JOIN ats_applicant_trackings t
+                    ON a.applicant_id = t.applicant_id
+                LEFT JOIN ats_applicant_progress p
+                    ON t.progress_id = p.progress_id
+                WHERE p.status = 'BLACKLISTED';
+    `;
 
+    try {
+        const [results, fields] = await pool.execute(sql);
+        return results
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+const checkInBlacklisted = (applicant, blackListedApplicants) => {
+    let isBlacklisted = false; 
+    blackListedApplicants.forEach(blacklisted => {
+
+        if (
+            applicant.first_name === blacklisted.first_name &&
+            applicant.last_name === blacklisted.last_name && 
+            applicant.email_1 === blacklisted.email_1 &&
+            applicant.mobile_number_1 === blacklisted.mobile_number_1
+        ) {
+            isBlacklisted = true; 
+            
+        }
+    });
+    return isBlacklisted; 
+}
 
 
 exports.checkIfBlacklisted = async (req, res) => {
